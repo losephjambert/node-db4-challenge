9ecbe576ef566e83d7aa754dc5bcd2fe9003cc2a
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-07 15:53:06 -0800

    feature: migrations: scaffolds recipes, ingredients, and instructions tables

--- data/migrations/20191107154137_bootstrap_recipes_db.js
+++ data/migrations/20191107154137_bootstrap_recipes_db.js
@@ -1,8 +1,25 @@
+exports.up = function (knex) {
+  return knex.schema
+    // recipes table
+    .createTable('recipes', recipes => {
+      recipes.increments();
+      recipes.string('recipe_name', 255).notNullable()
+    })
 
-exports.up = function(knex) {
-  
+    // ingredients table
+    .createTable('ingredients', ingredients => {
+      ingredients.increments()
+      ingredients.string('ingredient_name', 255).notNullable()
+    })
+
+    // instructions table
+    .createTable('instructions', instructions => {
+      instructions.increments()
+      instructions.integer('sequence_number').notNullable()
+      instructions.text('instruction_text', 500).notNullable()
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+
 };
