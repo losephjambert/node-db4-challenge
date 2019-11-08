8d3f429c90fc040e7d8190ed479718d64a6a27cf
Joseph Lambert <josephc.lambert@gmail.com>
2019-11-08 08:07:33 -0800

    adds more recipe queries

--- data/queries/recipe_queries.sql
+++ data/queries/recipe_queries.sql
@@ -1,1 +1,10 @@
-select * from instructions;
+-- select * from ingredients;
+-- select * from recipes;
+-- select * from recipe_ingredients;
+
+-- select ri.quantity, r.recipe_name, i.ingredient_name
+-- from recipe_ingredients as ri
+-- join ingredients as i
+-- join recipes as r
+-- where i.id = ri.ingredient_id
+-- and r.id = 4;
